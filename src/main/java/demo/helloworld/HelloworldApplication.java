ab1193dc617b6d388e41a1580a2cb31c8adeaf16
Gallinago1981 <snipes81b-github@yahoo.co.jp>
2020-01-22 16:59:09 +0900

    api を controller に分離

--- src/main/java/demo/helloworld/HelloworldApplication.java
+++ src/main/java/demo/helloworld/HelloworldApplication.java
@@ -2,24 +2,12 @@ package demo.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class HelloworldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
     }
 
-    @GetMapping(path = "/hello")
-    public String hello() {
-        return "Hello World - #4 issue";
-    }
-
-    @GetMapping(path = "/hello2")
-    public String hello2() {
-        return "Hello Java11";
-    }
 }
---
+++ src/main/java/demo/helloworld/controller/HelloController.java
@@ -0,0 +1,18 @@
+package demo.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+  @GetMapping(path = "/hello")
+  public String hello() {
+    return "Hello World - #4 issue";
+  }
+
+  @GetMapping(path = "/hello2")
+  public String hello2() {
+    return "Hello Java11";
+  }
+
+}

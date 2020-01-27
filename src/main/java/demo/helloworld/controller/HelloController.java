54a8e34a2dd3cf65886b44f5f6e69b52d0efd306
Gallinago1981 <snipes81b-github@yahoo.co.jp>
2020-01-27 15:11:52 +0900

    delete conflict test method

--- src/main/java/demo/helloworld/controller/HelloController.java
+++ src/main/java/demo/helloworld/controller/HelloController.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-  @GetMapping("/conflict")
-  public String conflict() {
-    return "Hello Conflict";
-  }
   @GetMapping(path = "/hello")
   public String hello() {
     return "Hello World - #4 issue";

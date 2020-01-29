7dea789805fbee4864c00bbc96b63e454ca69b8f
Gallinago1981 <snipes81b-github@yahoo.co.jp>
2020-01-29 17:30:07 +0900

    mod master

--- src/main/java/demo/helloworld/controller/HelloController.java
+++ src/main/java/demo/helloworld/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
   @GetMapping(path = "/hello")
   public String hello() {
-    return "Hello World from TOKYO";
+    return "Hello World from OSAKA";
   }
 
   @GetMapping(path = "/hello2")

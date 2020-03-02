d72abae2856da8b3038d79e7491a574b76f678cb
Gallinago1981 <snipes81b-github@yahoo.co.jp>
2020-03-02 12:07:37 +0900

    Add hello4 API

---
+++ src/main/java/demo/helloworld/service/MessageService.java
@@ -0,0 +1,16 @@
+package demo.helloworld.service;
+
+import demo.helloworld.type.MessageType;
+import org.springframework.stereotype.Service;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Service
+public class MessageService {
+
+  public String getMessage() {
+    long now = LocalDate.now().toEpochDay();
+    int index = BigDecimal.valueOf(now).remainder(BigDecimal.valueOf(3)).intValue();
+    return MessageType.values()[index].getMessage();
+  }
+}

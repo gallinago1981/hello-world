0084fc023e417932d0709db84a799b1dcd1e7102
Gallinago1981 <snipes81b-github@yahoo.co.jp>
2020-10-22 16:59:07 +0900

    Add Greeting Api

--- src/main/java/demo/helloworld/controller/HelloController.java
+++ src/main/java/demo/helloworld/controller/HelloController.java
@@ -1,5 +1,7 @@
 package demo.helloworld.controller;
 
+import static demo.helloworld.type.TimeZoneType.getCurrentTimeZone;
+
 import demo.helloworld.type.MessageType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,19 @@ public class HelloController {
   @GetMapping(path = "/hello4")
   public String hello4() {
     return "Hello 4th";
+  }
+
+  @GetMapping(path = "/greeting")
+  public String greeting() {
+    switch (getCurrentTimeZone()) {
+      case MORNING: return "Morning";
+      case NOON: return "Hello";
+      case EVENING: return "Good Bye.";
+      case NIGHT: return "Good Night";
+      case MIDNIGHT: return "Sleep soon";
+      default:
+        throw new IllegalStateException("Unexpected value: " + getCurrentTimeZone());
+    }
   }
 
 }
--- src/main/java/demo/helloworld/type/TimeZoneType.java
+++ src/main/java/demo/helloworld/type/TimeZoneType.java
@@ -1,14 +1,32 @@
 package demo.helloworld.type;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum TimeZoneType {
 
-  MORNING(1),
-  NOON(2),
-  EVENING(3);
+  MORNING(1, LocalTime.of(6, 0, 0), LocalTime.of(10, 59, 59)),
+  NOON(2, LocalTime.of(11, 0, 0), LocalTime.of(14, 59, 59)),
+  EVENING(3, LocalTime.of(16, 0, 0), LocalTime.of(18, 59, 59)),
+  NIGHT(4, LocalTime.of(19, 0, 0), LocalTime.of(21, 59, 59)),
+  MIDNIGHT(5, LocalTime.of(22, 0, 0), LocalTime.of(5, 59, 59));
 
   int value;
-  TimeZoneType(int value) {
+  LocalTime startTime;
+  LocalTime endTime;
+  TimeZoneType(int value, LocalTime startTime, LocalTime endTime) {
     this.value = value;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
+
+  public static TimeZoneType getCurrentTimeZone() {
+    LocalTime now = LocalDateTime.now().toLocalTime();
+    Optional<TimeZoneType> currentTimeZone =
+            Arrays.stream(TimeZoneType.values()).filter(t -> now.isAfter(t.startTime) && now.isBefore(t.endTime)).findFirst();
+    return currentTimeZone.orElseThrow();
   }
 
 }
